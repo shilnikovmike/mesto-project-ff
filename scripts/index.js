12743f570d39e9c8ce6dd278e5ed1d2b788c1a0a
Mikhail Shilnikov <shilinkovmike@yandex.ru>
2025-01-24 17:26:25 +0300

    #1 project version to review

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,29 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
+const cardContainer = document.querySelector('.places__list');
 
-// @todo: DOM узлы
+function addCard(cardItem, removeCard) {
+  const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardTitle = cardElement.querySelector('.card__title');
+  const removeButton = cardElement.querySelector('.card__delete-button');
+  cardImage.src = cardItem.link;
+  cardImage.alt = cardItem.name;
+  cardTitle.textContent = cardItem.name;
+  removeButton.addEventListener('click', () => {
+    removeCard(cardElement);
+  });
+  return cardElement;
+}
 
-// @todo: Функция создания карточки
+function renderCard(cardItem, removeCard) {
+  const newCard = addCard(cardItem, removeCard);
+  cardContainer.append(newCard);
+}
 
-// @todo: Функция удаления карточки
+initialCards.forEach((cardItem) => {
+  renderCard(cardItem, removeCard);
+});
 
-// @todo: Вывести карточки на страницу
+function removeCard(cardElement) {
+  cardElement.remove();
+}
